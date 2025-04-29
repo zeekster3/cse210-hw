ee513b80b49676d6cb5728e6dff5c6f65c668fa7
zeekster3 <zeeekster3@gmail.com>
2025-04-29 11:07:32 -0600

    modified:   README.md     modified:   sandbox/Sandbox/Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+    // This is a text
+    // This is a second change
+    Console.WriteLine("What is your first name? ");
+    string firstName = Console.ReadLine();
+     Console.WriteLine("What is your last name? ");
+    string lastName = Console.ReadLine();
+    Console.WriteLine($"Your name is {lastname}, {firstName}{lastName}.");
+    int x = 10;
     }
 }
