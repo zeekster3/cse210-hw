584ead34fad5149dcf387dce10a7caa784c40ddd
zeekster3 <zeeekster3@gmail.com>
2025-05-01 09:43:54 -0600

    new file:   prove/Develop02/Develop02.sln 	modified:   sandbox/Sandbox/Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,11 +8,30 @@ class Program
 
     // This is a text
     // This is a second change
-    Console.WriteLine("What is your first name? ");
-    string firstName = Console.ReadLine();
-     Console.WriteLine("What is your last name? ");
-    string lastName = Console.ReadLine();
-    Console.WriteLine($"Your name is {lastname}, {firstName}{lastName}.");
-    int x = 10;
+    //Console.WriteLine("What is your first name? ");
+    //string firstName = Console.ReadLine();
+    // Console.WriteLine("What is your last name? ");
+    //string lastName = Console.ReadLine();
+    //Console.WriteLine($"Your name is {lastname}, {firstName}{lastName}.");
+    //int x = 10;
+
+
+    // int x = 10;
+    // if(x == 10)
+    // {
+    //     Console.WriteLine("X is 10");
+    // }
+
+    // for(int i = 0; i < x; i++;){
+    //     Console.WriteLine("Bob is cool");
+    // }
+    // }
+
+    List(int) myNumbers = new List<int>();
+    mynumbers.add(10);
+    mynumbers.add(90);
+    mynumbers.add(43);
+
+    Console.WriteLine(mynumbers);
     }
 }
